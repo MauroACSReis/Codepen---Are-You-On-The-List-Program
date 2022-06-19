5e31df47356c3e2716a4bc1f2015c0ba2320e5f8
Mauro Reis <mauroacsreis@gmail.com>
2022-06-19 11:15:55 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// My answer
+
+function livesHere() {
+var name = prompt("What's your name");
+var list = ["Mauro", "Tatiana", "Luciele", "Rodrigo"];
+if (
+    list.includes(name)) {
+    alert("You live here");
+} else {
+    alert("You do not live here")
+}
+}
+    livesHere()
